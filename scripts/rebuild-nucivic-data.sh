791f7ff918dacc63a080a5c38974826cbd065840
Teofilo Sibileau <teo.sibileau@gmail.com>
2014-09-10 09:41:46 -0300

    Configuring new modules namespaces

--- scripts/rebuild-nucivic-data.sh
+++ scripts/rebuild-nucivic-data.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 cd ../docroot
-drush -y en nudata_disqus nudata_story nucivic_data
+drush -y en nudata_disqus data_story nucivic_data
 drush vset theme_default nucivic_data
 drush rr
-drush -y fr --force nudata_disqusw
-drush -y fr --force nudata_story
+drush -y fr --force nudata_disqus data_story
 drush cc all
