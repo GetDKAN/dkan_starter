f76bdb4a0156db16408291b0843e5112724749bd
Teofilo Sibileau <teo.sibileau@gmail.com>
2014-09-19 12:17:23 -0300

    + Updating rebuild scripts to rebuild permissions after install + Setting rebuild script to exit with the code of the command that failed + Mods to .travis build + Mods to .travis build + Mods to .travis build + Mods to .travis build + Mods to .travis build + Mods to .travis build + Mods to .travis build + Mods to .travis build

--- scripts/rebuild-nucivic-data.sh
+++ scripts/rebuild-nucivic-data.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
+set -e
 cd ../docroot
 drush -y en data_disqus data_story nucivic_data publishing_workflow og_permissions_settings
 drush -y dis dkan_sitewide_roles_perms
 drush vset theme_default nucivic_data
-drush rr
 drush -y fr --force data_disqus data_story publishing_workflow
+drush php-eval 'node_access_rebuild();'
+drush rr
 drush cc all
