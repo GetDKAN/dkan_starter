a452640c606eb08705ad7312a18a37ab47ffc9bc
Teofilo Sibileau <teo.sibileau@gmail.com>
2014-10-03 17:25:18 -0300

    reformating script

--- scripts/rebuild-nucivic-data.sh
+++ scripts/rebuild-nucivic-data.sh
@@ -2,7 +2,9 @@
 
 set -e
 cd ../docroot
-drush -y en data_disqus data_story nucivic_data publishing_workflow og_permissions_settings visualization_entity_choropleth_bundle visualization_entity_geojson_bundle
+drush -y en data_disqus data_story publishing_workflow og_permissions_settings
+drush -y visualization_entity_choropleth_bundle visualization_entity_geojson_bundle
+drush -y en nucivic_data
 drush -y dis dkan_sitewide_roles_perms
 drush vset theme_default nucivic_data
 drush -y fr --force data_disqus data_story publishing_workflow visualization_entity_choropleth_bundle visualization_entity_geojson_bundle
