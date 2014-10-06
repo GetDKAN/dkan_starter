b032da82d197565008a86c0db550d8831cca903e
Teofilo Sibileau <teo.sibileau@gmail.com>
2014-10-06 13:28:08 -0300

    + Frontpage Demo Front for NuCivic Data + Moving sandbox theme changes to product

--- scripts/rebuild-nucivic-data.sh
+++ scripts/rebuild-nucivic-data.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
 set -e
+
 cd ../docroot
-drush -y en data_disqus data_story publishing_workflow og_permissions_settings
-drush -y visualization_entity_choropleth_bundle visualization_entity_geojson_bundle
-drush -y en nucivic_data
+
+# Disable DKAN Modules we don't want
 drush -y dis dkan_sitewide_roles_perms
+drush -y dis dkan_sitewide_demo_front
+
+# Enable components
+drush -y en data_disqus data_story data_story_storyteller_role publishing_workflow og_permissions_settings
+drush -y en visualization_entity_choropleth_bundle visualization_entity_geojson_bundle
+
+# Enable NUCIVIC Data Front
+drush -y en nucivic_data_demo_front
+
+# Enable theme and set default
+drush -y en nucivic_data
 drush vset theme_default nucivic_data
-drush -y fr --force data_disqus data_story publishing_workflow visualization_entity_choropleth_bundle visualization_entity_geojson_bundle
+
+# Revert
+drush -y fr --force data_disqus data_story publishing_workflow
+drush -y fr --force visualization_entity_choropleth_bundle visualization_entity_geojson_bundle
+
+# Rebuild Permissions, Registry and clear Cache
 drush php-eval 'node_access_rebuild();'
 drush rr
 drush cc all
