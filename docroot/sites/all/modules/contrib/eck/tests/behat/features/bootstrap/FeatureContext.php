7694e9a46f1f366175690a38e38c802a71c29e73
Nansen <webadmin@nansen.nuamsdev.com>
2014-10-09 21:06:42 +0000

    Rebuild with drush buildmanager.

--- docroot/sites/all/modules/contrib/eck/tests/behat/features/bootstrap/FeatureContext.php
+++ docroot/sites/all/modules/contrib/eck/tests/behat/features/bootstrap/FeatureContext.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * @file
- * Custom behat step definitions.
- */
 
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\TranslatedContextInterface,
@@ -11,17 +7,38 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
 /**
  * Features context.
  */
-class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
-  /**
-   * Initializes context.
-   * 
-   * Every scenario gets its own context object.
-   *
-   * @param array $parameters 
-   *   Context parameters (set them up through behat.yml)
-   */
-  public function __construct(array $parameters) {}
+class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets its own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        // Initialize your context here
+    }
+
+//
+// Place your definition and hook methods here:
+//
+//    /**
+//     * @Given /^I have done something with "([^"]*)"$/
+//     */
+//    public function iHaveDoneSomethingWith($argument)
+//    {
+//        doSomethingWith($argument);
+//    }
+//
 }
