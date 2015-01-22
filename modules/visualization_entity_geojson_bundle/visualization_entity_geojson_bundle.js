245f45e48098e66bd9bf71c7934abb3903eec03b
Nansen <webadmin@nansen.nuamsdev.com>
2015-01-22 15:04:19 +0000

    Squashed 'projects/visualization_entity/' changes from 78f25b7..43703b9
    43703b9 Merge pull request #19 from NuCivic/nucivic_internal_263
    be14cc4 Adding zoomToGeometries behaviours to choropleth bundle
    a06ee2f Adding zoomToGeometries behaviours to geojson_bundle
    
    git-subtree-dir: projects/visualization_entity
    git-subtree-split: 43703b9de191405d5d5033da81c9efe067888bbf

--- modules/visualization_entity_geojson_bundle/visualization_entity_geojson_bundle.js
+++ modules/visualization_entity_geojson_bundle/visualization_entity_geojson_bundle.js
@@ -7,20 +7,6 @@
       var map = new L.map(div.get(0)).setView([37.8, -96], 4);
 
       var geojson = new L.GeoJSON(settings.geojson);
-      var bounds = geojson.getBounds();
-
-      for (var layer in geojson._layers) {
-        if (geojson._layers.hasOwnProperty(layer)){
-          var feature = geojson._layers[layer].feature;
-          for ( var j = 1; j < feature.geometry.coordinates[0].length; j++ ) {
-            var latlng = feature.geometry.coordinates[0][j];
-            latlng = new L.LatLng(latlng[1],latlng[0]);
-            bounds.extend(latlng);
-          }
-        }
-      }
-
-      map.fitBounds(bounds);
 
       var mapUrl = "//otile{s}-s.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png";
       var osmAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="//developer.mapquest.com/content/osm/mq_logo.png">';
@@ -28,6 +14,7 @@
 
       map.addLayer(bg);
       geojson.addTo(map);
+      map.zoomToGeometries(geojson);
     },
   };
 })(jQuery);
