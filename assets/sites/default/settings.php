96ba86748f7a9b0e87e05b7a79dccd4993b1337a
Frank Carey <fcarey@gmail.com>
2015-06-17 18:34:52 +0000

    Update the settings.php file so that it's mostly ready to go on acquia.

--- assets/sites/default/settings.php
+++ assets/sites/default/settings.php
@@ -1,30 +1,27 @@
 <?php
-/**
- * This settings.php file was created by the Acquia Cloud ah-site-archive-import
- * Drush command. The imported archive did not contain a settings.php file, so
- * the import process created this file by default. You can replace this file
- * with the standard default settings.php file for your version of Drupal.
- * However, be sure to keep the last line that loads the "Acquia Cloud settings
- * include file", which provides the correct database credentials for your site.
- */
-$update_free_access = FALSE;
-$drupal_hash_salt = '';
-ini_set('arg_separator.output',     '&amp;');
-ini_set('magic_quotes_runtime',     0);
-ini_set('magic_quotes_sybase',      0);
-ini_set('session.cache_expire',     200000);
-ini_set('session.cache_limiter',    'none');
-ini_set('session.cookie_lifetime',  2000000);
-ini_set('session.gc_divisor',       100);
-ini_set('session.gc_maxlifetime',   200000);
-ini_set('session.gc_probability',   1);
-ini_set('session.save_handler',     'user');
-ini_set('session.use_cookies',      1);
-ini_set('session.use_only_cookies', 1);
-ini_set('session.use_trans_sid',    0);
-ini_set('url_rewriter.tags',        '');
 
-$settings_local = DRUPAL_ROOT . '/' . conf_path() . '/settings.local.php';
-if (file_exists($settings_local)) {
-  include $settings_local;
+$conf['install_profile'] = 'dkan';
+$sitename = "somesite";
+
+// TODO: Make this file work on both Acquia and Pantheon.
+
+// On Acquia Cloud, this include file configures Drupal to use the correct
+// database in each site environment (Dev, Stage, or Prod). To use this
+// settings.php for development on your local workstation, set $db_url
+// (Drupal 5 or 6) or $databases (Drupal 7) as described in comments above.
+if (file_exists('/var/www/site-php')) {
+  require "/var/www/site-php/$sitename/$sitename-settings.inc";
 }
+
+// IMPORTANT. Local settings include comes first so we can fake acquia env variables on local.
+// See settings.local.demo.php for how to setup your local environment.
+if (!(function_exists('acquia_hosting_db_choose_active') || file_exists('/var/www/site-php'))) {
+  $settings_local = DRUPAL_ROOT . '/' . conf_path() . '/settings.local.php';
+  if (file_exists($settings_local)) {
+    include $settings_local;
+  }
+}
+
+/**
+ * Add Additional Custom Settings Below This Line.
+ */
