baf82cf5822e647ed09c34a7b655f0322a7f2326
Frank Carey <fcarey@gmail.com>
2015-06-29 23:12:08 +0000

    Issue NuCivic/internal#679: Remove the base_url parameter so that we can set it using run-tests.sh script. Clean up run-tests.sh.

--- run-tests.sh
+++ run-tests.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
+# Store the working directory so we can switch back.
 PWD=`pwd`
 echo $PWD
 cd tests
+
 if [ -z "$1" ];
 then
-  PROFILE="default"
+  echo "Usage: run-tests.sh http://127.0.0.1:8888 [--name='Name of scenario'] [--additional-behat-arg=something]"
+  exit 1
 else
-  PROFILE=$1
+  URL=$1
 fi
 
-CMD="bin/behat --colors --profile=$PROFILE features/ ${@:2}"
-echo "Running $CMD"
+# USE
+ENV_PARAMS='BEHAT_PARAMS='\''{"extensions":{"Behat\\MinkExtension":{"base_url":"'$URL'"}}}'\'
+# --expand doesn't work / was removed in behat 3.x See https://github.com/Behat/Behat/issues/723
+FORMAT_SETTINGS='--format-settings='\''{"expand": true}'\'
+CMD="bin/behat --colors features/ -v"
+echo "Running $ENV_PARAMS $CMD $FORMAT_SETTINGS ${@:2}"
 
-
-$CMD
+eval $ENV_PARAMS $CMD
 
 cd $PWD
