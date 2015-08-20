e84207fb8550ab8a8d21c0c91b1bbac5fed64e47
Frank Carey <fcarey@gmail.com>
2015-08-20 21:25:54 +0000

    NuCivic/internal#736: Set environment specific settings and turn on/off specific modules based on environment.

--- assets/sites/default/settings.php
+++ assets/sites/default/settings.php
@@ -22,6 +22,77 @@ if (!(function_exists('acquia_hosting_db_choose_active') || file_exists('/var/ww
   }
 }
 
+// Setup standard environments using devinci
+require DRUPAL_ROOT . "/sites/all/modules/contrib/devinci/devinci.environments.inc";
+devinci_set_env();
+
+/**
+ * Setup standard settings for all environments.
+ */
+$conf['install_profile'] = 'dkan';
+
+
 /**
  * Add Additional Custom Settings Below This Line.
+ *
+ * For environment switching, use environment.module and hook_environment_switch().
  */
+switch(ENVIRONMENT) {
+
+  case 'local':
+    $conf['features_master_temp_enabled_modules'] = array(
+      'maillog',
+      'dblog',
+      'devel',
+      'views_ui',
+      'acquia_purge',
+      'acquia_solr'
+    );
+    $conf['features_master_temp_disabled_modules'] = array(
+      'acquia_purge',
+      'acquia_solr'
+    );
+    break;
+
+  case 'dev':
+    $conf['features_master_temp_enabled_modules'] = array(
+      'maillog',
+      'dblog',
+      'devel',
+    );
+    break;
+
+  case 'test':
+    $conf['features_master_temp_enabled_modules'] = array(
+      'maillog',
+    );
+    break;
+
+  case 'live':
+    break;
+
+  default:
+    print ("ENVIRONMENT set to " . ENVIRONMENT . ", but not mapped in settings.php");
+    throw new Exception("ENVIRONMENT set to " . ENVIRONMENT . ", but not mapped in settings.php");
+}
+
+/**
+ * Function called by devinci when the environment switches if defined.
+ */
+function devinci_custom_environment_switch($target_env, $current_env) {
+
+  switch($target_env) {
+
+    case 'local':
+      break;
+
+    case 'dev':
+      break;
+
+    case 'test':
+      break;
+
+    case 'live':
+      break;
+  }
+}
