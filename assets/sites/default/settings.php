201413b49bc5cd45b3426a6756e3103948d86963
Frank Carey <fcarey@gmail.com>
2015-08-27 14:58:09 +0000

    NuCivic/internal#736: Remove acquia specific include to test if that's even necessary.

--- assets/sites/default/settings.php
+++ assets/sites/default/settings.php
@@ -1,17 +1,6 @@
 <?php
 
 $conf['install_profile'] = 'dkan';
-$sitename = "somesite";
-
-// TODO: Make this file work on both Acquia and Pantheon.
-
-// On Acquia Cloud, this include file configures Drupal to use the correct
-// database in each site environment (Dev, Stage, or Prod). To use this
-// settings.php for development on your local workstation, set $db_url
-// (Drupal 5 or 6) or $databases (Drupal 7) as described in comments above.
-if (file_exists('/var/www/site-php')) {
-  require "/var/www/site-php/$sitename/$sitename-settings.inc";
-}
 
 // IMPORTANT. Local settings include comes first so we can fake acquia env variables on local.
 // See settings.local.demo.php for how to setup your local environment.
