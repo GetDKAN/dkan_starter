ed61772723c9d45b6e0c5d4d0ff6f9a5529572ca
Teofilo Sibileau <teo.sibileau@gmail.com>
2015-12-02 12:17:34 -0300

    + Adds registry rebuild and drush cache clear to acquia hooks

--- hooks/common/post-code-update/drush-env-switch.sh
+++ hooks/common/post-code-update/drush-env-switch.sh
@@ -8,5 +8,7 @@ env=$2
 drush_alias=$site'.'$env
 target_env=`drush @$drush_alias php-eval "echo ENVIRONMENT;"`
 
+drush @$drush_alias rr
+drush cc drush
 drush @$drush_alias env-switch $target_env --force 
 drush @$drush_alias -y updb
