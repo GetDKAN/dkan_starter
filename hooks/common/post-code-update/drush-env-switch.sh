d13254f6cb2958adbf7a33321cb9479cbd3d90d3
Teofilo Sibileau <teo.sibileau@gmail.com>
2016-02-26 09:53:56 -0300

    Issue #CIVIC-1537: Makes deploy hook Check for installation

--- hooks/common/post-code-update/drush-env-switch.sh
+++ hooks/common/post-code-update/drush-env-switch.sh
@@ -8,8 +8,15 @@ env=$2
 drush_alias=$site'.'$env
 target_env=`drush @$drush_alias php-eval "echo ENVIRONMENT;"`
 
-drush @$drush_alias rr
-drush cc drush
-drush @$drush_alias -y fr --force custom_config
-drush @$drush_alias env-switch $target_env --force 
-drush @$drush_alias -y updb
+drupal=$(drush @$drush_alias status | grep -e "Drupal bootstrap" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+if [[ "$drupal" =~ "Successful" ]]; then
+  echo "Installation detected, running deploy script"
+  drush @$drush_alias rr
+  drush cc drush
+  drush @$drush_alias -y fr --force custom_config
+  drush @$drush_alias env-switch $target_env --force
+  drush @$drush_alias -y updb
+else
+  echo "Installation not detected"
+fi
