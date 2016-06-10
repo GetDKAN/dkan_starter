2ea577cb15b9e79a3f44f0f7da04478950a4068c
Teofilo Sibileau <teo.sibileau@gmail.com>
2016-06-10 15:13:45 -0300

    Smarter deploy hook that detects if acquia solr integration module is available

--- hooks/common/post-code-update/drush-env-switch.sh
+++ hooks/common/post-code-update/drush-env-switch.sh
@@ -13,10 +13,19 @@ drupal=$(drush @$drush_alias status | grep -e "Drupal bootstrap" | sed -e 's/^[[
 if [[ "$drupal" =~ "Successful" ]]; then
   echo "Installation detected, running deploy script"
   drush @$drush_alias rr
-  drush cc drush
+  drush @$drush_alias cc drush
   drush @$drush_alias -y fr --force custom_config
   drush @$drush_alias env-switch $target_env --force
   drush @$drush_alias -y updb
+  DB_BASED_SEARCH=`drush @$drush_alias pmi dkan_acquia_search_solr | grep disabled`
+  if [ -z "$DB_BASED_SEARCH" ]; then
+    echo "SOLR Search, avoiding indexing data"
+  else
+    drush @$drush_alias search_api_index search-api-index datasets
+    drush @$drush_alias search_api_index search-api-index groups_di
+    drush @$drush_alias search_api_index search-api-index stories_index
+    echo "DB Search, indexing data"
+  fi
 else
   echo "Installation not detected"
 fi
