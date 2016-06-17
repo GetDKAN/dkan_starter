25f5f341d962d5509564867110d756489c3cd2f1
Teofilo Sibileau <teo.sibileau@gmail.com>
2016-06-17 15:47:50 -0300

    + Replaces base url conf in acquia settings + Only leaves solr search available on acquia production environments.

--- assets/sites/default/settings.nucivic.php
+++ assets/sites/default/settings.nucivic.php
@@ -9,6 +9,19 @@
 switch(ENVIRONMENT) {
   case 'development':
   case 'test':
+    // Remove this if you are certain you have multiple cores on an acquia site.
+    if (!isset($conf['features_master_temp_disabled_modules'])) {
+      $conf['features_master_temp_disabled_modules'] = array();
+    }
+    $conf['features_master_temp_disabled_modules'] = array_merge(
+      $conf['features_master_temp_disabled_modules'],
+      array(
+        'search_api_solr',
+        'search_api_acquia',
+        'dkan_acquia_search_solr',
+      )
+    );
+    break;
   case 'production':
     // Add this back when we split backup and monitoring functionality in 
     // $conf['features_master_temp_enabled_modules'] = array_merge(
