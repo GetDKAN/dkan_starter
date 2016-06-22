b1a1e19bca7af84b653d276a6ae7f4f046e23e24
dkinzer <dtkinzer@gmail.com>
2016-06-21 22:17:07 -0400

    Make settings more configurable.
    REF CIVIC-3019
    
    pro vs enterprise settings...

--- config/config.php
+++ config/config.php
@@ -12,17 +12,30 @@ $conf = array (
   ),
   'acquia' => 
   array (
+    'subscription' => 'professional',
     'dev' => 
     array (
       'base_url' => 'http://devurl',
+      'core_id' => 'changeme',
+      'derived_key' => 'changeme',
     ),
     'test' => 
     array (
       'base_url' => 'http://testurl',
+      'core_id' => 'changeme',
+      'derived_key' => 'changeme',
+    ),
+    'ra' => 
+    array (
+      'base_url' => 'http://testurl',
+      'core_id' => 'changeme',
+      'derived_key' => 'changeme',
     ),
     'prod' => 
     array (
       'base_url' => 'http://produrl',
+      'core_id' => 'changeme',
+      'derived_key' => 'changeme',
     ),
   ),
 );
