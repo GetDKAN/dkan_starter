563e1faf1917f90f0b89be9e4e307ea255e8aa5b
dkinzer <dtkinzer@gmail.com>
2016-11-18 12:16:38 -0500

    Do not delete all nodes.
    Deleting all nodes can cause some builds to stall (HHS)  plus we determined that
    this does not help test that was failing.  Also it's duplicate work that was
    being done in sanitize step on jenkins job.

--- .ahoy/site/.scripts/deploy.sh
+++ .ahoy/site/.scripts/deploy.sh
@@ -1,11 +1,3 @@
 eval $(ahoy parse config/config.yml)
 drush --root=docroot user-password 1 --password="$private_probo_password"
 drush --root=docroot user-password admin --password="$private_probo_password"
-
-if [ "$CI" = true ]; then
-  drush --root=docroot search-api-clear
-  drush --root=docroot php-script .ahoy/site/.scripts/delete-all-nodes.php
-  drush --root=docroot search-api-enable
-fi
-
-
