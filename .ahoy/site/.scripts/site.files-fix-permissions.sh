579fb21f430b44c0d0fec0039c153564d5ed0b53
David Kinzer <dtkinzer@gmail.com>
2016-12-13 10:29:57 -0500

    Make assets/sites/default writable locally. (#35)
    Fixes error thrown locally due to folder permissions.
    
    REF CIVIC-4787

--- .ahoy/site/.scripts/site.files-fix-permissions.sh
+++ .ahoy/site/.scripts/site.files-fix-permissions.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p docroot/sites/default/files
+chmod 777 assets/sites/default
 
 if [ -d docroot/sites/default/files ]; then
   find docroot/sites/default/files/ -type d -exec chmod o+rwx {} \;
