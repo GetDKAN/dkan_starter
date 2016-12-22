3094d12d48787da4a55ce0f8833a07795e72a7d6
Teofilo Sibileau <teo.sibileau@gmail.com>
2016-12-22 11:29:21 -0300

    Adds support for private files if available in s3 backup

--- .ahoy/site/.scripts/site.files-fix-permissions.sh
+++ .ahoy/site/.scripts/site.files-fix-permissions.sh
@@ -7,3 +7,10 @@ if [ -d docroot/sites/default/files ]; then
   find docroot/sites/default/files/ -type d -exec chmod o+rwx {} \;
   find docroot/sites/default/files/ -type f -exec chmod o+rw {} \;
 fi
+
+mkdir -p docroot/sites/default/private
+
+if [ -d docroot/sites/default/private]; then
+  find docroot/sites/default/private/ -type d -exec chmod o+rwx {} \;
+  find docroot/sites/default/private/ -type f -exec chmod o+rw {} \;
+fi
