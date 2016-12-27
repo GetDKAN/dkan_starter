9797b23f1e31814e135ae708270c3a4d3a93f960
Teofilo Sibileau <teo.sibileau@gmail.com>
2016-12-27 12:17:16 -0300

    remove mkdir docroot/sites/default/private

--- .ahoy/site/.scripts/site.files-fix-permissions.sh
+++ .ahoy/site/.scripts/site.files-fix-permissions.sh
@@ -8,8 +8,6 @@ if [ -d docroot/sites/default/files ]; then
   find docroot/sites/default/files/ -type f -exec chmod o+rw {} \;
 fi
 
-mkdir -p docroot/sites/default/private
-
 if [ -d docroot/sites/default/private]; then
   find docroot/sites/default/private/ -type d -exec chmod o+rwx {} \;
   find docroot/sites/default/private/ -type f -exec chmod o+rw {} \;
