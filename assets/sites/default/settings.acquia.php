de9227bc9d1ec6fce810e06d20d9bc28b54cdd61
David Kinzer <dtkinzer@gmail.com>
2017-03-10 15:32:45 -0500

    Conditionally increase memory when  odfe is enabled. (#95)
    * Conditionally increase memory when  odfe is enabled.
    
    REF CIVIC-4036
    
    * Conditionally increase memory for datastore endpoint
    
    * Clean code for increase memory limit for some paths

--- assets/sites/default/settings.acquia.php
+++ assets/sites/default/settings.acquia.php
@@ -50,5 +50,26 @@ if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
       'derived_key' => $conf['acquia'][$env]['derived_key'],
     );
   }
+
+  // {{{1 Conditionally manage memory.
+  if (isset($conf['default']['odfe']) && $conf['default']['odfe']['enabled']) {
+    $high_memory_paths = array(
+      'admin',
+      'node/add',
+      'node/%node/edit',
+      'node/%node/moderation',
+      'file/ajax',
+      'api/action/datastore/search.json'
+    );
+
+    // Standarize node edit paths for validation.
+    $current_path = preg_replace("/\d+/", '%node', $_GET['q']);
+    foreach ($high_memory_paths as $high_memory_path) {
+      if ((strpos($current_path, $high_memory_path) === 0)) {
+        ini_set('memory_limit', '512M');
+      }
+    }
+  }
+
   acquia_hosting_db_choose_active();
 }
