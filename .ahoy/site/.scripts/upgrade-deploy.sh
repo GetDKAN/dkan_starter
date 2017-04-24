c0cf282e9ccc1d05d5e0d4a88eaa0cee49bcd98b
Riadh Habbachi <riadh.habbachi@gmail.com>
2017-04-24 22:37:54 +0100

    Get the exact upgrade_1_13 value

--- .ahoy/site/.scripts/upgrade-deploy.sh
+++ .ahoy/site/.scripts/upgrade-deploy.sh
@@ -3,7 +3,7 @@ echo "Target environment is $target_env"
 echo "Drush alias is $drush_alias"
 
 upgrade_version=upgrade_1_13
-upgrade_status=`drush @$drush_alias vget $upgrade_version --pipe`
+upgrade_status=`drush @$drush_alias vget $upgrade_version --exact`
 
 if [ "$upgrade_status" != 'upgraded' ]; then
   target_env=$target_env drush_alias=$drush_alias bash .ahoy/site/.scripts/upgrades/$upgrade_version.sh
