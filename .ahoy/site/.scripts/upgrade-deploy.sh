4474481b397ee2d9c45bd0f798bf8ad2175b99f7
Dharizza <dharizza@gmail.com>
2017-04-25 08:17:00 -0600

    Remove unnecessary quotes on upgrade-deploy.sh.

--- .ahoy/site/.scripts/upgrade-deploy.sh
+++ .ahoy/site/.scripts/upgrade-deploy.sh
@@ -5,7 +5,7 @@ echo "Drush alias is $drush_alias"
 upgrade_version=upgrade_1_13
 upgrade_status=`drush @$drush_alias vget $upgrade_version --exact`
 
-if [ "$upgrade_status" != 'upgraded' ]; then
+if [ $upgrade_status != 'upgraded' ]; then
   target_env=$target_env drush_alias=$drush_alias bash .ahoy/site/.scripts/upgrades/$upgrade_version.sh
   drush @$drush_alias vset $upgrade_version upgraded
 fi
