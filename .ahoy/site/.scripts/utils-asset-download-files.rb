d5019d557a7e382e9beef8fd6bd0d9cc90f7fc89
Mariano Carballal <marianocarballal@gmail.com>
2017-07-26 17:01:42 -0300

    Fix site variable interpolation during asset download (#399)

--- .ahoy/site/.scripts/utils-asset-download-files.rb
+++ .ahoy/site/.scripts/utils-asset-download-files.rb
@@ -5,7 +5,7 @@ site = `ahoy utils name`
 aws_url = CONFIG["private"]["aws"]["scrubbed_data_url"]
 asset = "#{aws_url}/#{site}.prod.files.tar.gz"
 
-`LC_TIME=en_US.UTF-8 perl .ahoy/site/.scripts/s3curl.pl --id local #{asset} > backups/$site.prod.files.tar.gz`
+`LC_TIME=en_US.UTF-8 perl .ahoy/site/.scripts/s3curl.pl --id local #{asset} > backups/ #{site}.prod.files.tar.gz`
 
 puts ""
 puts "Unpacking the files asset."
