36c0565f60ea8bd4211f6837200def524edd1204
Kristian Ducharme <kducharm@3072643.no-reply.drupal.org>
2017-12-13 22:55:34 -0800

    Fixed settings.docker.demo.php for Docker Compose v2, memcached server settings

--- assets/sites/default/settings.docker.demo.php
+++ assets/sites/default/settings.docker.demo.php
@@ -1,15 +1,15 @@
 <?php
 
-if (getenv('DB_1_ENV_MYSQL_DATABASE')) {
-  $databases = array (
+if (getenv('DB_1_ENV_MYSQL_DATABASE') != FALSE || getenv('MYSQL_DATABASE') != FALSE) {
+  $databases = array(
     'default' =>
-    array (
+    array(
       'default' =>
-      array (
-        'database' => getenv('DB_1_ENV_MYSQL_DATABASE'),
-        'username' => getenv('DB_1_ENV_MYSQL_USER'),
-        'password' => getenv('DB_1_ENV_MYSQL_PASSWORD'),
-        'host' => getenv('DB_1_PORT_3306_TCP_ADDR'),
+      array(
+        'database' => (getenv('MYSQL_DATABASE') != FALSE) ? getenv('MYSQL_DATABASE') : getenv('DB_1_ENV_MYSQL_DATABASE'),
+        'username' => (getenv('MYSQL_USER') != FALSE) ? getenv('MYSQL_USER') : getenv('DB_1_ENV_MYSQL_USER'),
+        'password' => (getenv('MYSQL_PASSWORD') != FALSE) ? getenv('MYSQL_PASSWORD') : getenv('DB_1_ENV_MYSQL_PASSWORD'),
+        'host' => (getenv('DB_1_PORT_3306_TCP_ADDR') != FALSE) ? getenv('DB_1_PORT_3306_TCP_ADDR') : 'db',
         'port' => '',
         'driver' => 'mysql',
         'prefix' => '',
@@ -17,7 +17,7 @@ if (getenv('DB_1_ENV_MYSQL_DATABASE')) {
     ),
   );
 
-  # Workaround for permission issues with NFS shares in Vagrant
+  // Workaround for permission issues with NFS shares in Vagrant.
   $conf['file_chmod_directory'] = 0777;
   $conf['file_chmod_file'] = 0666;
 
@@ -32,8 +32,13 @@ if (getenv('DB_1_ENV_MYSQL_DATABASE')) {
   }
 }
 
-if (getenv('MEMCACHED_PORT_11211_TCP_ADDR')) {
-  $memcache_server = getenv('MEMCACHED_PORT_11211_TCP_ADDR') . ':' . getenv('MEMCACHED_PORT_11211_TCP_PORT');
+if (getenv('HOSTNAME') == 'web' || getenv('MEMCACHED_PORT_11211_TCP_ADDR') != FALSE) {
+  if (getenv('MEMCACHED_PORT_11211_TCP_ADDR') != FALSE) {
+    $memcache_server = getenv('MEMCACHED_PORT_11211_TCP_ADDR') . ':' . getenv('MEMCACHED_PORT_11211_TCP_PORT');
+  }
+  else {
+    $memcache_server = 'memcached:11211';
+  }
   $conf['memcache_servers'] = array($memcache_server => 'default');
   $conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
   $conf['cache_default_class'] = 'MemCacheDrupal';
