42b210e8335dc77e41c1012a9b487ae72356a053
acouch <acinternets@gmail.com>
2018-05-16 14:48:55 -0400

    Updates hooks

--- hooks/common/post-code-update/drush-env-switch.sh
+++ hooks/common/post-code-update/drush-env-switch.sh
@@ -16,10 +16,11 @@ env_map=(
   "lt:test"
 )
 
+echo "The environment is $env"
 for m in "${env_map[@]}"; do
   target_key=$(echo $m | cut -d: -f1)
   if [ "$target_key" = "$env" ]; then
-  echo $m
+    echo $m
     target_env=$(echo $m | cut -d: -f2)
    break
   fi
@@ -30,7 +31,7 @@ acquia=`env | grep AH_REALM`
 if [ "$acquia" != '' ]; then
   echo "Acquia environment detected. Moving to correct directory. /var/www/html/$1'.'$2"
   cd /var/www/html/$1'.'$2
-  PWD
+  pwd 
 fi
 
 echo "target_env is $target_env"
